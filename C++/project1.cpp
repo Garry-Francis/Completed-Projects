f18d53adbffbdc013baf7f283eef23eba2ea0210
Garry-Francis <122326829+Garry-Francis@users.noreply.github.com>
2026-02-09 19:23:49 -0500

    Update dice rolling logic to sum two dice

--- C++/project1.cpp
+++ C++/project1.cpp
@@ -29,13 +29,16 @@ int main() {
     // in their corresponding index on the array
     cout << "Simulating " << numRolls << " rolls..." << endl;
     for (int i = 0; i < numRolls; i++) {
-        int rolledNum = rand() % 11 + 2;
-        arrRolledDice[rolledNum] = arrRolledDice[rolledNum] + 1;
+        // Generate two random numbers between 1 and 6
+        int die1 = rand() % 6 + 1;
+        int die2 = rand() % 6 + 1;
+
+        int rolledNum = die1 + die2; // Sum of two dice rolls
+        arrRolledDice[rolledNum]++;  // Increment the count for this sum
     }
 
-    // Prints the contents of the array with the counted numbers rolled
     cout << "Results:" << endl;
-    for (int i = 2; i < 13; i++) {
+    for (int i = 2; i <= 12; i++) { // Dice sums range from 2 to 12
         cout << i << " was rolled " << arrRolledDice[i] << " times" << endl;
     }
 
